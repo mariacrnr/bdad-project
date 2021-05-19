2636c30775042978a7ae045f2f6990930a3c09d1
Maria Carneiro <mariajcarneiro56@gmail.com>
2021-05-19 18:59:01 +0100

    deleted comment

--- third-delivery/queries/int2.sql
+++ third-delivery/queries/int2.sql
@@ -15,13 +15,3 @@ WHERE tipo LIKE "VIP" AND BI NOT IN(
 
 --Não se precisa de verificar se a reserva está associada a algum Lounge dado que se o membro não estiver na tabela de reservas,
 --também não tem uma reserva associada a algum Lounge.
-
--- Número de Reservas por cada Lounge de cada Discoteca (OUTRA QUERIE MAS FEITA PARA EFEITOS DE TESTE)
-/*SELECT idLounge, COUNT(*) FROM Pessoa 
-JOIN Membro USING(BI) 
-JOIN Reserva USING(BI) JOIN ReservaLounge JOIN Lounge 
-WHERE Reserva.id = ReservaLounge.idReserva 
-AND Lounge.id == ReservaLounge.idLounge 
-AND Membro.idDiscoteca = Lounge.idDiscoteca 
-AND tipo LIKE "VIP"
-GROUP BY Membro.idDiscoteca, idLounge;*/
