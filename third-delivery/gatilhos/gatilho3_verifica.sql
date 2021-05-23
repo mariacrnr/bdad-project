2819edf8ca29f8559672dd1e9598b1e45ac914c4
Edgar Torre <up201906573@fe.up.pt>
2021-05-23 18:20:55 +0100

    Trigger 3 finished (?)

--- third-delivery/gatilhos/gatilho3_verifica.sql
+++ third-delivery/gatilhos/gatilho3_verifica.sql
@@ -3,3 +3,13 @@
 .nullvalue NULL
 
 PRAGMA foreign_keys = ON;
+
+INSERT INTO Discoteca VALUES (11, 'Kiss', 'Algarve', 'Hugo Gonçalves', 9500); -- Discoteca nº11
+INSERT INTO Funcionario VALUES (9000, 'Alfredo Conceição', 914548326, 57483133, 'Rua dos Anjos', 1000, 11);
+INSERT INTO Funcionario VALUES (9001, 'Pedro Albuquerque', 914548327, 57483134, 'Rua dos Anjos', 900, 11);
+
+SELECT id, salario FROM Funcionario WHERE Funcionario.id = 9000 OR Funcionario.id = 9001;
+
+INSERT INTO Hierarquia(idSubalterno, idGerente) VALUES (9000, 9001);
+
+SELECT id, salario FROM Funcionario WHERE Funcionario.id = 9000 OR Funcionario.id = 9001; 
