03dc894b1874b63081ff21a7c3f794df95a7f7c7
Edgar Torre <up201906573@fe.up.pt>
2021-05-23 17:39:11 +0100

    Trigger 1 WIP

--- third-delivery/gatilhos/gatilho1_adiciona.sql
+++ third-delivery/gatilhos/gatilho1_adiciona.sql
@@ -8,19 +8,19 @@
 -- statements;
 --END;
 
+--SELECT * FROM Reserva JOIN Lounge ON Lounge.idDiscoteca = NEW.idLounge.idDiscoteca <> (Membro.idDiscoteca com NEW.idReserva.BI)
+
+
 /* Apenas membros VIPs de uma discoteca podem reservar um lounge nessa discoteca */
 CREATE TRIGGER reservar_lounge_exclusivo_membros_VIP
     AFTER INSERT ON ReservaLounge
     FOR EACH ROW
-    WHEN 
-        SELECT * 
-        FROM    (SELECT * 
-                FROM ReservaLounge 
-                    JOIN Lounge
-                WHERE ReservaLounge.idLounge = NEW.idLounge;) AS Lounge
-            JOIN     
+    WHEN (SELECT COUNT(*) FROM Membro 
+            WHERE Membro.BI = (SELECT BI FROM Reserva WHERE id = NEW.idReserva;) 
+            AND Membro.idDiscoteca = (SELECT idDiscoteca FROM Lounge WHERE id = NEW.idLounge;);)
+        = 0
 
-        SELECT * FROM Reserva JOIN Lounge ON Lounge.idDiscoteca = NEW.idLounge.idDiscoteca <> (Membro.idDiscoteca com NEW.idReserva.BI)
+
     -- Se a pessoa referenciada por Reserva.BI NÃO for membro VIP na discoteca do Lounge
 BEGIN
     DELETE FROM ReservaLounge
