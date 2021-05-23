8e4c14838b0c89d0db25234ad2ed6aee6e8fe2c0
Edgar Torre <up201906573@fe.up.pt>
2021-05-23 17:07:05 +0100

    Trigger 1 WIP

--- third-delivery/gatilhos/gatilho1_adiciona.sql
+++ third-delivery/gatilhos/gatilho1_adiciona.sql
@@ -12,8 +12,17 @@
 CREATE TRIGGER reservar_lounge_exclusivo_membros_VIP
     AFTER INSERT ON ReservaLounge
     FOR EACH ROW
-    WHEN NEW.idLounge.idDiscoteca <> (Membro.idDiscoteca com NEW.idReserva.BI)
+    WHEN 
+        SELECT * 
+        FROM    (SELECT * 
+                FROM ReservaLounge 
+                    JOIN Lounge
+                WHERE ReservaLounge.idLounge = NEW.idLounge;) AS Lounge
+            JOIN     
+
+        SELECT * FROM Reserva JOIN Lounge ON Lounge.idDiscoteca = NEW.idLounge.idDiscoteca <> (Membro.idDiscoteca com NEW.idReserva.BI)
     -- Se a pessoa referenciada por Reserva.BI NÃO for membro VIP na discoteca do Lounge
 BEGIN
-    -- remover inserção
+    DELETE FROM ReservaLounge
+    WHERE ReservaLounge.idReserva = NEW.idReserva AND ReservaLounge.idLounge;
 END;
